64eb9d3aab7779c4a52ebb8291c6f0aa17f1d46b
Lameck Meshack <rhinolamer@gmail.com>
2023-04-20 10:38:37 +0300

    implemented create task

--- src/pages/CreateTask.vue
+++ src/pages/CreateTask.vue
@@ -1,86 +1,138 @@
 <template>
-  <div
-    class="block max-w-md rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
-  >
-    <form>
-      <!--Name input-->
-      <div class="relative mb-6" data-te-input-wrapper-init>
-        <input
-          type="text"
-          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          id="exampleInput7"
-          placeholder="Name"
-        />
-        <label
-          for="exampleInput7"
-          class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >Name
-        </label>
+  <form>
+    <div class="mb-4 mx-auto px-2 w-6/12">
+      <label class="block mb-1 text-sm" for="input2">Task Name</label>
+
+      <input
+        v-model="task.name"
+        id="input2"
+        class="w-full border px-4 py-2 rounded focus:border focus:border-blue-500 focus:shadow-outline outline-none"
+        type="text"
+        placeholder="Input placeholder..."
+      />
+    </div>
+    <div class="mb-4 flex mx-auto px-2 w-6/12">
+      <div class="mb-4 mx-auto w-6/12">
+        <label class="block mb-1 text-sm" for="select-item">Status</label>
+        <div class="relative">
+          <select
+            class="w-full border bg-white px-4 pr-8 py-2 rounded focus:border-blue-200 focus:shadow-outline outline-none appearance-none"
+            id="select-item"
+            v-model="task.status_id"
+          >
+            <option value="1">Not Start</option>
+            <option value="2">In Progress</option>
+            <option value="3">Completed</option>
+          </select>
+
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+          >
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
 
-      <!--Email input-->
-      <div class="relative mb-6" data-te-input-wrapper-init>
-        <input
-          type="email"
-          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          id="exampleInput8"
-          placeholder="Email address"
-        />
-        <label
-          for="exampleInput8"
-          class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >Email address
-        </label>
+      <div class="mb-4 mx-auto pl-2 w-6/12">
+        <label class="block mb-1 text-sm" for="select-item">Due Date</label>
+        <div class="relative">
+          <VueDatePicker class="pb-2" v-model="task.due_date"></VueDatePicker>
+        </div>
       </div>
+    </div>
 
-      <!--Message textarea-->
-      <div class="relative mb-6" data-te-input-wrapper-init>
-        <textarea
-          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          id="exampleFormControlTextarea13"
-          rows="3"
-          placeholder="Message"
-        ></textarea>
-        <label
-          for="exampleFormControlTextarea13"
-          class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >Message
-        </label>
-      </div>
+    <div class="mb-4 mx-auto px-2 w-6/12">
+      <label class="block mb-1 text-sm" for="textarea1">Task Description</label>
 
-      <!--Checkbox-->
-      <div
-        class="mb-6 flex min-h-[1.5rem] items-center justify-center pl-[1.5rem]"
-      >
-        <input
-          class="relative float-left -ml-[1.5rem] mr-[6px] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-          type="checkbox"
-          value=""
-          id="exampleCheck10"
-        />
-        <label
-          class="inline-block pl-[0.15rem] hover:cursor-pointer"
-          for="exampleCheck10"
-        >
-          Send me a copy of this message
-        </label>
-      </div>
-
-      <!--Submit button-->
+      <textarea
+        id="textarea1"
+        v-model="task.description"
+        class="w-full border px-4 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none"
+        rows="5"
+        placeholder="Task Description..."
+      ></textarea>
+    </div>
+    <!-- buton -->
+    <div class="mb-4 mx-auto px-2 w-6/12">
       <button
-        type="submit"
-        class="dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-        data-te-ripple-init
-        data-te-ripple-color="light"
+        @click="onSubmit"
+        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
       >
-        Send
+        Create Task
       </button>
-    </form>
+    </div>
+  </form>
+  <div class="mb-4 mx-auto px-2 w-6/12">
+    <success
+      v-if="success"
+      :msg="msg"
+      :success="success"
+      @close="setSuccess(false)"
+    />
+
+    <error v-if="error" :msg="msg" :error="error" @close="setError(false)" />
   </div>
 </template>
 <script>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import TaskServices from "../services/task.service";
+import Success from "@/components/Success.vue";
+import Error from "@/components/Error.vue";
 export default {
-    name: 'ContactForm',    
-    
-}
+  name: "ContactForm",
+  components: { VueDatePicker, Success, Error },
+  data() {
+    return {
+      success: false,
+      error: false,
+      msg: "",
+      task: {
+        name: "",
+        description: "",
+        status_id: "",
+        due_date: "",
+      },
+    };
+  },
+  methods: {
+    setSuccess(value) {
+      this.success = value;
+    },
+    async onSubmit() {
+      const hasEmptyValue = Object.values(this.task).some(
+        (value) => value === ""
+      );
+      if (hasEmptyValue) {
+        this.error = true;
+        this.msg = "Please fill all the fields";
+        return;
+      }
+      try {
+        //format due_date in task
+        this.task.due_date = new Date(this.task.due_date)
+          .toISOString()
+          .replace("T", " ")
+          .slice(0, -5);
+        const res = await TaskServices.create(this.task);
+        this.success = true;
+        this.error = false;
+        this.msg = res.data.message;
+        // this.$router.push("/dashboard");
+      } catch (error) {
+        this.error = true;
+        this.msg = "Something went wrong";
+      }
+    },
+  },
+};
 </script>
